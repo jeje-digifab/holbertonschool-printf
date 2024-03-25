b1abea8a8c96be45feef917a4532815277f72237
jeje-digifab <j.romand.digifab@gmail.com>
2024-03-25 13:11:21 +0100

    Initial file main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+
+int _printf(const char *format, ...);
+
+
+
+#endif /* !=MAIN_H_ */
